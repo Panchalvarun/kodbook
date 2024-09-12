1a0caed84bd2d5002256503cddf4204df4515029
Panchalvarun <varunpanchal51@gmail.com>
2024-09-12 18:47:26 +0530

    First commit

---
+++ com.kodbook/src/main/java/com/kodbook/services/PostServiceImplementation.java
@@ -0,0 +1,30 @@
+package com.kodbook.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kodbook.entities.Post;
+import com.kodbook.repositories.PostRepository;
+
+@Service
+public class PostServiceImplementation implements PostService{
+
+	@Autowired
+	PostRepository repo;
+	
+	
+	public void createPost(Post post) {
+		repo.save(post);
+		
+		
+	}
+
+
+	@Override
+	public List<Post> fetchAllPosts() {
+		return repo.findAll();
+	}
+
+}
